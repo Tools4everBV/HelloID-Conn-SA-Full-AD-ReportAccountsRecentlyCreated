675ab7f4cbafb60cfaffb929064fa36ea717b73b
Michiel85 <68066270+Michiel85@users.noreply.github.com>
2021-01-14 09:22:31 +0100

    Bugfix creating data sources

--- All-in-one setup/createform.ps1
+++ All-in-one setup/createform.ps1
@@ -306,7 +306,7 @@ $tmpInput = @'
 
 '@ 
 $dataSourceGuid_0 = [PSCustomObject]@{} 
-Invoke-HelloIDDatasource -DatasourceName "AD-user-generate-table-report-recently-created" -DatasourceType "4" -DatasourceInput $tmpInput -DatasourcePsScript -$tmpPsScript -DatasourceModel $tmpModel -returnObject ([Ref]$dataSourceGuid_0) 
+Invoke-HelloIDDatasource -DatasourceName "AD-user-generate-table-report-recently-created" -DatasourceType "4" -DatasourceInput $tmpInput -DatasourcePsScript $tmpPsScript -DatasourceModel $tmpModel -returnObject ([Ref]$dataSourceGuid_0) 
 <# End: DataSource "AD-user-generate-table-report-recently-created" #>
 <# End: HelloID Data sources #>
 
